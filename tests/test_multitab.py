6922c2a301ecb491b523c6c3633cf635fce5c691
astromancer <hannes@saao.ac.za>
2022-06-23 11:15:31 +0200

    TST: cleanup tests

--- tests/test_multitab.py
+++ tests/test_multitab.py
@@ -4,43 +4,27 @@ import sys
 # third-party
 import numpy as np
 
-
 # local
 from mpl_multitab import MplMultiTab, QtWidgets
 
+#
 sys.modules['matplotlib.pyplot'] = None
 
-# def test_multitab():
-# fig, ax = plt.subplots()
-# ax.plot(np.random.randn(100, 2), 'mp')
 
-# fig2, ax2 = plt.subplots()
-# ax2.plot(np.random.randn(100, 2), 'bx')
+def test_multitab(n=100, colours='rgb'):
+    # Example use for MplMultiTab
+    # Create a scatter plot of `n` random xy-points for each colour
+    ui = MplMultiTab()
+    for c in colours:
+        fig = ui.add_tab(c)
+        ax = fig.subplots()
+        ax.scatter(*np.random.randn(2, n), color=c)
 
-# ui = MplMultiTab(None, [fig], [])
-# ui.show()
+    return ui
 
 
-# Example use
-
-
-# if __name__ == '__main__':
-app = QtWidgets.QApplication(sys.argv)
-
-n = 100
-colours = 'rgb'
-ui = MplMultiTab()
-for c in colours:
-    fig = ui.add_tab(c)
-    ax = fig.subplots()
-    ax.scatter(*np.random.randn(2, n), color=c)
-
-ui.show()
 if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ui = test_multitab()
+    ui.show()
     sys.exit(app.exec_())
-
-# from IPython import embed
-# embed()
-# sys.exit(app.exec_())
-
-# main()
--- tests/test_multitab_2d.py
+++ tests/test_multitab_2d.py
@@ -9,24 +9,27 @@ import numpy as np
 # local
 from mpl_multitab import MplMultiTab2D, QtWidgets
 
+#
 sys.modules['matplotlib.pyplot'] = None
 
-# Example use
 
-# This dataset is equal number observations per dataset. This need not be the
-# case in general.
-app = QtWidgets.QApplication(sys.argv)
-ui = MplMultiTab2D()
+def test_multitab_2d(n=100, colours='rgb', markers='123'):
+    # Example use for MplMultiTab2D
+    # This dataset is equal number observations per dataset. This need not be the
+    # case in general.
+    
+    ui = MplMultiTab2D()
 
-n = 100
-colours = 'rgb'
-markers = '123'
-for c, m in itt.product(colours, markers):
-    fig = ui.add_tab(f'Dataset {c.upper()}', f'Observation {m}')
-    ax = fig.subplots()
-    ax.scatter(*np.random.randn(2, n), color=c, marker=f'${m}$')
+    for c, m in itt.product(colours, markers):
+        fig = ui.add_tab(f'Dataset {c.upper()}', f'Observation {m}')
+        ax = fig.subplots()
+        ax.scatter(*np.random.randn(2, n), color=c, marker=f'${m}$')
 
-ui.show()
+    return ui
+
 
 if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    ui = test_multitab_2d()
+    ui.show()
     sys.exit(app.exec_())
